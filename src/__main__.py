086afa433db86f5d9d9d995dcb0b83e976168b93
DamienCM <45606435+DamienCM@users.noreply.github.com>
2025-01-15 16:11:50 +0100

    try except in whole main

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,22 @@
-from config.logger_setup import setup_logging
-setup_logging()  # Call this once at app startup
-print("Loading tk UI : ...")
-from ui.ui import initialiser_interface_tk
-print("Loading tk UI : DONE")
-print("Loading Calulator : ...")
-from calculateur import CalculateurFraisLivraison
-print("Loading Calulator : DONE")
-print("Loading qt UI  : ... ")
-from ui.ui_qt import initialize_qt_interface
-print("Loading qt UI  : DONE ")
+try: 
+    from config.logger_setup import setup_logging
+    setup_logging()  # Call this once at app startup
+    print("Loading tk UI : ...")
+    from ui.ui import initialiser_interface_tk
+    print("Loading tk UI : DONE")
+    print("Loading Calulator : ...")
+    from calculateur import CalculateurFraisLivraison
+    print("Loading Calulator : DONE")
+    print("Loading qt UI  : ... ")
+    from ui.ui_qt import initialize_qt_interface
+    print("Loading qt UI  : DONE ")
 
 
-if __name__ == "__main__":
-    print("Welcome to Transport Calculator 2025 !")
-    calculateur = CalculateurFraisLivraison()
-    initialize_qt_interface(calculateur)
-    initialiser_interface_tk(calculateur)
+    if __name__ == "__main__":
+            print("Welcome to Transport Calculator 2025 !")
+            calculateur = CalculateurFraisLivraison()
+            initialize_qt_interface(calculateur)
+            initialiser_interface_tk(calculateur)
+except Exception as e:
+    print(f'[ERROR] Fatal error programm will stop \n{e}')
+    input("...")
